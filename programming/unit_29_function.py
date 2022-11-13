b45a35491f0e512e966b444e0fcc78555afa0b43
Seongbae103 <seongbae103@gmail.com>
2022-11-13 18:22:45 +0900

    11-13

--- programming/unit_29_function.py
+++ programming/unit_29_function.py
@@ -1,9 +1,15 @@
-def solution_1():
-    pass
+#연습
+x=10
+y=3
+def get_quotient_remainder(x, y):
+    return x//y, x % y
+quotient, remainder = get_quotient_remainder(x, y)
+print('몫 : {0}, 나머지 : {1}'.format(quotient, remainder))
 
-def solution_2():
-    pass
+#심사
+x, y = map(int, input().split())
+def calc(x, y):
+    return x+y, x-y, x*y, x/y
 
-if __name__ == '__main__':
-    print(solution_1())
-    print(solution_2())
+a, s, m, d = calc(x, y)
+print('덧셈 : {0}, 뺄셈 : {1}, 곱셈 : {2}, 나눗셈 : {3}'.format(a, s, m, d))
